6681735d3db0767cb9c1c07fb9c56230d3fa15e7
Obada Kadri <abu3antar@gmail.com>
2012-12-30 17:42:42 -0800

    removed contact form and added a contact link on the navbar

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$('.contact').click(function(e){
+		e.preventDefault()
+		$(location).attr('href','mailto:okadri@live.com?body=' + encodeURIComponent("\n\n\n ~ Sent from ObadaKadri.com "));
+	});
+});
