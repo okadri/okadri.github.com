0cb11003c35ad203df7bdfd6fd5ba8c2362e611e
Obada Kadri <abu3antar@gmail.com>
2012-12-30 23:25:51 -0800

    implemented scrollspy and affix sidebar

--- js/custom.js
+++ js/custom.js
@@ -3,4 +3,5 @@ $(document).ready(function() {
 		e.preventDefault()
 		$(location).attr('href','mailto:okadri@live.com?body=' + encodeURIComponent("\n\n\n ~ Sent from ObadaKadri.com "));
 	});
+	$('.sidebar-nav').scrollspy()
 });
