4b1b6d49d459e8500525ab4f79fe1c9045cd565a
Obada Kadri <abu3antar@gmail.com>
2012-12-31 22:51:08 -0800

    added about me

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,14 @@ $(document).ready(function() {
 		e.preventDefault()
 		$(location).attr('href','mailto:okadri@live.com?body=' + encodeURIComponent("\n\n\n ~ Sent from ObadaKadri.com "));
 	});
+	$('.linkedin').click(function(e){
+		e.preventDefault()
+		$(location).attr('href','http://www.linkedin.com/in/obadakadri');
+	});
+	$('.gplus').click(function(e){
+		e.preventDefault()
+		$(location).attr('href','https://plus.google.com/100772236268733566326');
+	});
 	$('.sidebar-nav').scrollspy()
 	$('.app-icon').click(function(e){
 		e.preventDefault()
