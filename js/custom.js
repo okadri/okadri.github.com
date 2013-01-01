433603c7734bbe805d93bc0dd5c5d6e591324383
Obada Kadri <abu3antar@gmail.com>
2012-12-31 18:47:58 -0800

    Google Play link to Lease Miles Widget

--- js/custom.js
+++ js/custom.js
@@ -4,4 +4,8 @@ $(document).ready(function() {
 		$(location).attr('href','mailto:okadri@live.com?body=' + encodeURIComponent("\n\n\n ~ Sent from ObadaKadri.com "));
 	});
 	$('.sidebar-nav').scrollspy()
+	$('.android').click(function(e){
+		e.preventDefault()
+		$(location).attr('href', $('.android').data('url'));		
+	});
 });
