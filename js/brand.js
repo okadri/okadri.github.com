0f739f28e5e4d2a845be0abfd2e5627ee54f365d
Obada Kadri <abu3antar@gmail.com>
2013-01-25 12:21:46 -0800

    changed to footer instead of .brand

--- js/brand.js
+++ js/brand.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('footer').html('<div class="brand">obadakadri.com</brand>');
+	$('.region-footer').html('<div class="brand">obadakadri.com</brand>');
 	$('.brand').click(function(e){
 		e.preventDefault()
 		$(location).attr('href','http://obadakadri.com');
