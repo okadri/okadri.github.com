4d1531de341498b24eb75536a65517a459025bd0
Obada Kadri <abu3antar@gmail.com>
2013-01-25 12:30:44 -0800

    changed brand-region to id

--- js/brand.js
+++ js/brand.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('.brand-region').append('<div class="brand">obadakadri.com</brand>');
+	$('#brand-region').append('<div class="brand">obadakadri.com</brand>');
 	$('.brand').click(function(e){
 		e.preventDefault()
 		$(location).attr('href','http://obadakadri.com');
