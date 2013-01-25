2c218f8489e62de32d4fe80d43bd46d9b9bbe044
Obada Kadri <abu3antar@gmail.com>
2013-01-25 12:26:53 -0800

    changed class to brand-region

--- js/brand.js
+++ js/brand.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('.region-footer').append('<div class="brand">obadakadri.com</brand>');
+	$('.brand-region').append('<div class="brand">obadakadri.com</brand>');
 	$('.brand').click(function(e){
 		e.preventDefault()
 		$(location).attr('href','http://obadakadri.com');
